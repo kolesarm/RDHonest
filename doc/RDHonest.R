2901e1ff641af27def5309ad30a9447dabd7fc72
Michal Kolesár <kolesarmi@googlemail.com>
2019-07-12 17:28:41 -0400

    Update manual

--- doc/RDHonest.R
+++ doc/RDHonest.R
@@ -106,7 +106,7 @@ for (j in unique(d$cell)){
 
 ## ------------------------------------------------------------------------
 RDHonest(log(earnings)~yearat14, cutoff=1947, h=5, data=cghs, M=0.1, se.method=c("EHW", "nn"))
-RDHonest(y~x, cutoff=1947, weights=weights, h=5, data=dd, M=0.1)
+RDHonest(y~x, cutoff=1947, weights=weights, h=5, data=dd, M=0.1, se.method=c("EHW", "nn"))
 
 ## ------------------------------------------------------------------------
 ## Assumes first column in the data frame corresponds to outcome,
